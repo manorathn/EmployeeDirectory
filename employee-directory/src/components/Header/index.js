6a368533fbd8804ee30e37f7a0a10f49c31c0c77
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-05-16 17:26:07 -0700

    updateHeader&Search

--- employee-directory/src/components/Header/index.js
+++ employee-directory/src/components/Header/index.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React from "react"
 
-const Header = () => {
+function Header() {
     return (
-        <h1 className="Header">Employee Directory</h1>
-    );
-};
+        <header className="bg-dark text-white p-3 text-center">
+            <h1 className="p-2 m-2">Employee Directory</h1>
+            <p>Click on "Phone Number" to sort numerically or "Email" to sort alphabetically.</p>
+        </header>
+    )
+}
 
 export default Header;
--- employee-directory/src/components/Search/index.js
+++ employee-directory/src/components/Search/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-
-const SearchBar = (props) => {
+function Search(props) {
     return (
-        <form>
-            <div className="form-group">
-                <input name="search" type="text" className="form-control" placeholder="Search Employee Name" id="search"
+        <form className="row">
+            <div className="text-center align-middle form-group mb-4">
+                <input
+                    onChange={props.handleInputChange}
+                    value={props.search}
+                    name="search"
+                    type="text"
+                    className="form-control col mx-auto w-50 text-center"
+                    placeholder="Search for an Employee"
+                    id="search"
                 />
             </div>
         </form>
     );
-};
+}
 
-export default SearchBar;
+export default Search;
